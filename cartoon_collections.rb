2378a16bdcd3cc728df120085b6d877289a5e9b1
Laura Berge <lauraeberge@gmail.com>
2019-08-29 23:43:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   newArray = []
-  newArray.concat(planeteer_calls).capitalize
+  newArray = planeteer_calls.capitalize
   puts newArray.collect {|x| x + "!"}
 end
 
