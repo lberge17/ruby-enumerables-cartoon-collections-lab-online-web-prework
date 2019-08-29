af0bd82f0de0e5d542bc112768c3c9b2c14f7a13
Laura Berge <lauraeberge@gmail.com>
2019-08-29 23:47:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|x| x + "!"}
+  planeteer_calls.collect do |calls|
+    puts "#{calls.capitalize}!"
+  end
   
 end
 
